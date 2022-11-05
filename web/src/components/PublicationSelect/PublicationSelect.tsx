0c93dd3016b709d28264fba6d996674a6bcdebc4
stevenzg <guoyinfeng@live.com>
2022-11-05 19:43:29 +0800

    fix: remove Chinese

--- web/src/components/PublicationSelect/PublicationSelect.tsx
+++ web/src/components/PublicationSelect/PublicationSelect.tsx
@@ -42,14 +42,16 @@ export default function PublicationSelect({ defaultValue, onChange }) {
     >
       {({ open }) => (
         <>
-          <Listbox.Label className="block text-sm font-medium text-gray-700">
+          <Listbox.Label className="mt-5 block text-sm font-medium text-gray-700">
             Publication
           </Listbox.Label>
           <div className="relative mt-1">
             <Listbox.Button className="relative w-full cursor-default rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 text-left shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm">
               <span className="flex items-center">
                 <span className="ml-3 block truncate">
-                  {selectedPublication ? selectedPublication.name : '请选择'}
+                  {selectedPublication
+                    ? selectedPublication.name
+                    : 'Select Publication'}
                 </span>
               </span>
               <span className="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
